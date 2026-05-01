0e5e8427a7a0b7d5d59471a2e8e4d1670c7771b6
Kingidk <kingidk@kingidk.net>
2026-05-01 12:17:00 -0400

    WIP Feature: Create Command

--- src/main/java/net/kingidk/commandMaker/commands/AdminCommand.java
+++ src/main/java/net/kingidk/commandMaker/commands/AdminCommand.java
@@ -9,6 +9,7 @@ import net.kyori.adventure.text.format.TextDecoration;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.Configuration;
 import org.jetbrains.annotations.NotNull;
 
 public class AdminCommand implements CommandExecutor {
@@ -19,9 +20,9 @@ public class AdminCommand implements CommandExecutor {
     }
 
     @Override
-    public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String @NotNull [] args) {
-        if (!commandSender.hasPermission("commandmaker.admin")) {
-            commandSender.sendMessage(Component.text("You do not have permission!", NamedTextColor.RED));
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String s, @NotNull String @NotNull [] args) {
+        if (!sender.hasPermission("commandmaker.admin")) {
+            sender.sendMessage(Component.text("You do not have permission!", NamedTextColor.RED));
             return true;
         }
 
@@ -39,16 +40,42 @@ public class AdminCommand implements CommandExecutor {
                     .appendNewline()
                     .append(link);
 
-            commandSender.sendMessage(message);
+            sender.sendMessage(message);
             return true;
         }
 
-        if (args.length == 1 && args[0].equalsIgnoreCase("reload")) {
-            plugin.reload();
-            commandSender.sendMessage(Component.text("CommandMaker has been reloaded!", NamedTextColor.GREEN));
-            return true;
+
+        switch (args[0].toLowerCase()) {
+            case "reload" -> reloadCommand(sender);
+            case "create" -> createCommand(sender, args);
         }
+
 
         return false;
     }
+
+
+    public boolean reloadCommand(CommandSender sender) {
+        plugin.reload();
+        sender.sendMessage(Component.text("CommandMaker has been reloaded!", NamedTextColor.GREEN));
+        return true;
+    }
+
+    public void createCommand(CommandSender sender, String[] args>) {
+        String name = args[1];
+        String type = args[2];
+
+        Configuration config = plugin.getConfig();
+        String commandSection = "commands." + args[1];
+            config.createSection(commandSection);
+            config.createSection(commandSection + ".type");
+            config.set(commandSection + ".type", args[1]);
+            if (args[2].equalsIgnoreCase("STRING")) {
+                config.createSection("")
+            }
+
+
+        sender.sendMessage(Component.text("Added command \"" + args[1] + "\""));
+    }
+
 }
