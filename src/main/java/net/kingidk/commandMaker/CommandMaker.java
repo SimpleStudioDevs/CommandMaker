b3c96ebc033b98be152f9f49ca1c7cb348ca0db5
Kingidk <kingidk@kingidk.net>
2026-05-16 08:49:27 -0400

    Revert "Half Baked Conditions"
    This reverts commit ac0e189d247eb7e754d4bde54b6ab9a168141d1f.

--- src/main/java/net/kingidk/commandMaker/CommandMaker.java
+++ src/main/java/net/kingidk/commandMaker/CommandMaker.java
@@ -3,14 +3,11 @@ package net.kingidk.commandMaker;
 import net.kingidk.commandMaker.arguments.ArgsDefinition;
 import net.kingidk.commandMaker.commandcreation.CustomCommand;
 import net.kingidk.commandMaker.commands.AdminCommand;
-import net.kingidk.commandMaker.conditions.ConditionsDefinition;
-import net.milkbowl.vault.economy.Economy;
 import org.bstats.bukkit.Metrics;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandMap;
 import org.bukkit.configuration.ConfigurationSection;
-import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.*;
@@ -18,8 +15,7 @@ import java.util.*;
 public final class CommandMaker extends JavaPlugin {
     private final List<CustomCommand> registeredCommands = new ArrayList<>();
     public boolean papi;
-    public boolean vault;
-    public static Economy econ = null;
+    public static CommandMaker instance;
 
 
     @Override
@@ -30,6 +26,7 @@ public final class CommandMaker extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        instance = this;
         // BStats
         final int PLUGINID = 31020;
         new Metrics(this, PLUGINID);
@@ -53,13 +50,6 @@ public final class CommandMaker extends JavaPlugin {
             getLogger().info("PlaceholderAPI not detected, PAPI-based placeholders will be in plain-text!");
             papi = false;
         } else papi = true;
-
-        var vaultPlugin = Bukkit.getPluginManager().getPlugin("Vault");
-
-        if (vaultPlugin == null) {
-            getLogger().info("Vault not detected, {balance} placeholder will be unavailable");
-            vault = false;
-        } else vault = true;
 
         Objects.requireNonNull(getCommand("commandmaker")).setExecutor(new AdminCommand(this));
         Objects.requireNonNull(getCommand("commandmaker")).setTabCompleter(new AdminCommand(this) {});
@@ -100,21 +90,8 @@ public final class CommandMaker extends JavaPlugin {
                 }
             }
 
-            ConfigurationSection conditionsSection = getConfig().getConfigurationSection("commands." + cmdName + ".conditions");
-            List<ConditionsDefinition> conditionDefs = new ArrayList<>();
-            if (conditionsSection != null) {
-                for (String conditionName : conditionsSection.getKeys(false)) {
-                    String variable =  conditionsSection.getString(conditionName + ".variable");
-                    double min = conditionsSection.getDouble(conditionName + ".min");
-                    double max = conditionsSection.getDouble(conditionName + ".max");
-                    String message = conditionsSection.getString(conditionName + ".message");
-                    conditionDefs.add(new ConditionsDefinition(variable, min, max, message));
-                }
-            }
-
-
             // Register built command to the server
-            CustomCommand cmd = new CustomCommand(cmdName, aliases, actions, this, permission, argDefs, conditionDefs);
+            CustomCommand cmd = new CustomCommand(cmdName, aliases, actions, this, permission, argDefs);
             commandMap.register(getName(), cmd);
 
             // Force custom commands to take highest priority — overwrite any conflicting registration under the bare name
@@ -146,26 +123,6 @@ public final class CommandMaker extends JavaPlugin {
 
     public Set<String> getCommandKeys() {
         return Objects.requireNonNull(getConfig().getConfigurationSection("commands")).getKeys(false);
-    }
-
-
-    private boolean setupEconomy() {
-        if (getServer().getPluginManager().getPlugin("Vault") == null) {
-            return false;
-        }
-
-        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
-        if (rsp == null) {
-            return false;
-        }
-
-        econ = rsp.getProvider();
-
-        return true;
-
-
-
-
     }
 
 
--- src/main/java/net/kingidk/commandMaker/conditions/ConditionsDefinition.java
+++
@@ -1,3 +0,0 @@
-package net.kingidk.commandMaker.conditions;
-
-public record ConditionsDefinition(String variable, double min, double max, String message) {}
